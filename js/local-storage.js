7d3fd169f0920be56e51c25919c76115e2f40b1c
Bonface Mudiri <mudirib@gmail.com>
2017-08-26 10:27:09 +0300

    Update NoteTaker

--- js/local-storage.js
+++ js/local-storage.js
@@ -1,26 +1,37 @@
 $(document).ready(function(){
+  getNotes() //gets all items of NoteTaker in localStorage
 
+  //Making textarea to slide up or down on click states
   $('#Note').click(function(){
     if($('#Note').attr('data-click-state') == 1){
       $('#Note').attr('data-click-state', 0)
       $("#NoteBox").slideUp();
-      getNotes()
-      writeNotes()
     } else {
       $('#Note').attr('data-click-state', 1)
       $("#NoteBox").slideDown();
     }
   })
 
+  //Save data automatically to localStorage while user is typing. Set delay is 1 second
+  var timeout;
+  $("#NoteBox").on("input propertychange change", function(){
+    console.log("changing");
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      writeNotes();
+    }, 1000);
+  });
+
+//Save to localStorage
   function writeNotes(){
     var notes = $("#NoteTextArea").val();
     localStorage.setItem("notes", notes)
   }
 
+//Retrieve from localStorage
   function getNotes(){
-
-      var localText = localStorage.getItem("notes")
+    var localText = localStorage.getItem("notes")
+    $("#NoteTextArea").html(localText)
   }
 
-
-})
+}) //End of document.ready()
